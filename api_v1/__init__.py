925b97724dcfc1127d04ebefb19b95b256dfb14c
FarrukhYuldoshev <communityfarrukh@gmail.com>
2025-02-18 18:33:09 +0500

    some changes

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from .teachers import router as teacher_router
 from .research_interests import router as research_interests_router
 from .educations import router as education_router
 from .work_experience import router as work_experience_router
 from .publications import router as publications_router
+from pathlib import Path
+from starlette.responses import FileResponse
 
 router = APIRouter(prefix="/api/v1")
+
+
+@router.get("/{file_path:path}")
+async def get_file(file_path: str):
+    url_file = Path(file_path)
+    if url_file.exists() and url_file.is_file():
+        return FileResponse(url_file)
+    raise HTTPException(status_code=404, detail="File not found")
+
+
 router.include_router(teacher_router)
 router.include_router(research_interests_router)
 router.include_router(publications_router)
--- api_v1/teachers/views.py
+++ api_v1/teachers/views.py
@@ -6,7 +6,6 @@ from fastapi import (
     Path,
     HTTPException,
 )
-from fastapi.responses import FileResponse
 from core.settings import db_sessions
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
@@ -16,16 +15,16 @@ from .schemas import (
     GetTeachersWithResearchInterests,
     UpdateTeacher,
 )
-from pathlib import Path as Pathlib
 from . import crud
 import uuid
 from demo_auth import get_active_user
+
 router = APIRouter(prefix="/teachers", tags=["Teachers API"])
 
 
 @router.post("/", response_model=GetTeachers, status_code=status.HTTP_201_CREATED)
 async def create_teacher(
-    user = Depends(get_active_user),
+    user=Depends(get_active_user),
     data=Depends(CreateTeacher),
     session: AsyncSession = Depends(db_sessions.session_dependency),
 ):
@@ -55,7 +54,7 @@ async def update_teacher(
     teacher_id: Annotated[uuid.UUID, Path(description="Teacher uuid")],
     data: UpdateTeacher = Depends(UpdateTeacher),
     session: AsyncSession = Depends(db_sessions.session_dependency),
-    user = Depends(get_active_user),
+    user=Depends(get_active_user),
 ):
     result = await crud.update_teacher(
         teacher_id=teacher_id, data=data, session=session
@@ -67,31 +66,7 @@ async def update_teacher(
 async def delete_teacher(
     teacher_id: uuid.UUID,
     session: AsyncSession = Depends(db_sessions.session_dependency),
-    user = Depends(get_active_user),
+    user=Depends(get_active_user),
 ):
     teacher = await crud.get_teacher_or_none(teacher_id=teacher_id, session=session)
     await crud.delete_teacher(teacher=teacher, session=session)
-
-@router.get("/get-image/")
-async def get_image(file_path: str):
-    url_file = Pathlib(file_path)
-    if url_file.exists() and url_file.is_file():
-        return FileResponse(url_file)
-    raise HTTPException(status_code = 404, detail="File not found")
-
-# import re
-#
-# pattern = r"^(?:(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])|(\d{4}))$"
-#
-# def identify_date_format_and_value(input_str):
-#     match = re.match(pattern, input_str)
-#     if match:
-#         if match.group(1):  # Agar yyyy-mm-dd formati mos kelsa
-#             year = match.group(1)
-#             month = match.group(2)
-#             day = match.group(3)
-#             return {"format": "yyyy-mm-dd", "value": f"{year}-{month}-{day}"}
-#         elif match.group(4):  # Agar faqat yyyy formati mos kelsa
-#             year = match.group(4)
-#             return {"format": "yyyy", "value": year}
-#     return {"format": "No match", "value": None}  # Mos kelmasa
